f280d0d06814d975ea878772b9309f37e7f7c59c
Tony Lee <leegreen123@naver.com>
2025-01-02 16:15:37 +0900

    Include tmuxinator for brew

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 
-# Set the DOTFILES directory
-DOTFILES=$HOME/dotfiles
 
 # Brew dependencies
 BREW_DEPENDENCIES=(
@@ -18,20 +16,8 @@ BREW_DEPENDENCIES=(
   tmux
   neovim
   stow
+  tmuxinator
 )
-
-# Stow folders
-STOW_FOLDERS="nvim,tmux,zsh,git,tmuxinator"
-
-# Back up existing files/folders
-backup_existing() {
-  local target=$1
-  if [[ -e $target || -L $target ]]; then
-    local backup="${target}_old_$(date +%Y%m%d%H%M%S)"
-    echo "Backing up $target to $backup"
-    mv "$target" "$backup"
-  fi
-}
 
 # Install Homebrew dependencies
 echo "Installing Homebrew dependencies..."
@@ -44,6 +30,33 @@ for package in $BREW_DEPENDENCIES; do
   fi
 done
 
+DOTFILES=$HOME/dotfiles
+BACKUP_DIR=$HOME/.dotfiles_old
+
+# Ensure the backup directory exists
+mkdir -p $BACKUP_DIR
+
+# List of files and folders to back up
+FILES_TO_BACKUP=(
+  ".config/nvim"
+  ".config/tmuxinator"
+  ".tmux.conf"
+  ".zshrc"
+)
+
+# Stow folders
+STOW_FOLDERS="nvim,tmux,zsh,tmuxinator"
+
+# Back up existing files and folders
+echo "Backing up existing dotfiles..."
+for item in "${FILES_TO_BACKUP[@]}"; do
+  if [[ -e $HOME/$item ]]; then
+    echo "Backing up $item to $BACKUP_DIR"
+    mv $HOME/$item $BACKUP_DIR/
+  fi
+done
+
+
 # Ensure DOTFILES directory exists
 if [[ ! -d $DOTFILES ]]; then
   echo "Error: DOTFILES directory $DOTFILES does not exist!"
@@ -53,12 +66,9 @@ fi
 # Symlink dotfiles using GNU Stow
 echo "Symlinking dotfiles with GNU Stow..."
 pushd $DOTFILES >/dev/null
-for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
+for folder in nvim tmux zsh tmuxinator; do
   echo "Processing $folder..."
-  for file in $(stow --no --verbose=2 $folder 2>&1 | grep -o "WARNING! .* already exists" | awk '{print $2}'); do
-    backup_existing "$HOME/$file"
-  done
-  stow -D $folder  # Remove any existing symlinks
+  stow -D $folder  # Remove existing symlinks
   stow $folder     # Create new symlinks
 done
 popd >/dev/null
